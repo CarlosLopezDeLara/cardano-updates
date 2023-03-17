d7a83a1e529b8f2e942a74de3822cf319749f054
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-03-17 09:22:56 +0000

    deploy: 1b754805d6984f2891a215a7123ae63db0b0061a

---
+++ assets/js/61d299e1.76807eb6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[5552],{88947:a=>{a.exports=JSON.parse('{"label":"release","permalink":"/cardano-updates/tags/release","allTagsPath":"/cardano-updates/tags","count":5}')}}]);
---
+++ assets/js/6c658908.11ad0754.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[3875],{18860:e=>{e.exports=JSON.parse('{"permalink":"/cardano-updates/tags/sre","page":1,"postsPerPage":5,"totalPages":1,"totalCount":2,"blogDescription":"Regular updates from Cardano Development Teams","blogTitle":"Cardano Development Updates"}')}}]);
