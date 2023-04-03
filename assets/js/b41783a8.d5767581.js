e3485021bc951a2901d2834432a7b9c5e1e3a0c6
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-04-03 10:03:33 +0000

    deploy: 12663bc19504c8a436b988a379a267afd5e3228c

---
+++ assets/js/b41783a8.d5767581.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[9104],{861:e=>{e.exports=JSON.parse('{"permalink":"/cardano-updates/tags/goedel","page":1,"postsPerPage":5,"totalPages":1,"totalCount":5,"blogDescription":"Regular updates from Cardano Development Teams","blogTitle":"Cardano Development Updates"}')}}]);
