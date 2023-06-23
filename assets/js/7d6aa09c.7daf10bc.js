d016c7093804d4999f847d4a88df5b26c3984263
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-06-23 12:51:19 +0000

    deploy: b9a572150c03f9583a940efd8bc120dd3b251eec

---
+++ assets/js/7d6aa09c.7daf10bc.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[18058],{18069:a=>{a.exports=JSON.parse('{"label":"goedel","permalink":"/cardano-updates/tags/goedel","allTagsPath":"/cardano-updates/tags","count":11}')}}]);
