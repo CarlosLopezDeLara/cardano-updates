f017c18fa90d919dba52ad39249f1bbf7156e307
Arnaud Bailly <arnaud.bailly@iohk.io>
2023-09-04 13:39:49 +0000

    deploy: ef901504ce9e863ef17f5c269fadc19bcaba4e6e

---
+++ assets/js/49db7061.db909d7b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcardano_updates=self.webpackChunkcardano_updates||[]).push([[60183],{853:a=>{a.exports=JSON.parse('{"label":"ledger","permalink":"/cardano-updates/tags/ledger","allTagsPath":"/cardano-updates/tags","count":23}')}}]);
